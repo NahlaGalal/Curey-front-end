31138bc596c6aaaf98719107a2c46b542736449e
Mohamed Zayan <m7mdzayan@gmail.com>
2019-12-03 22:59:49 +0200

    Button comp is added

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import NavigationBar from "../components/NavigationBar";
+import Button from "../components/Button";
 
 export class Home extends Component {
   render() {
     return (
       <div>
         <NavigationBar />
+        <div className="hb">
+          <Button size="btn-lg" color="btn-green">
+            click me
+          </Button>
+        </div>
       </div>
     );
   }
