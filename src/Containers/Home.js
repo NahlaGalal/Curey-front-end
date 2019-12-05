d343dade56b83c0fc7297c18999a2f5af618bbad
Mohamed Zayan <m7mdzayan@gmail.com>
2019-12-05 15:42:19 +0200

    doctor card is almost done

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
 import Navbar from "../components/Nav";
+import DoctorCard from "../components/DoctorCard";
 
 export class Home extends Component {
   render() {
@@ -10,7 +11,35 @@ export class Home extends Component {
         <NavigationBar />
 
         <Navbar />
-        <button className="btn btn-green btn-lg">bla bla</button>
+        <section className="g">
+          <DoctorCard
+            name="Mo Zayan"
+            price={129 + " L.E"}
+            speciality="Pediatric Surgery"
+          />
+          <DoctorCard
+            name="Mo Zayan"
+            price={129 + " L.E"}
+            speciality="Pediatric Surgery"
+          />
+          <DoctorCard
+            name="Mo Zayan"
+            price={129 + " L.E"}
+            speciality="Pediatric Surgery"
+          />
+          <DoctorCard
+            name="Mo Zayan"
+            price={129 + " L.E"}
+            speciality="Pediatric Surgery"
+          />
+          <DoctorCard
+            name="Mo Zayan"
+            price={129 + " L.E"}
+            speciality="Pediatric Surgery"
+          />
+        </section>
+
+        {/* <button className="btn btn-green btn-lg">bla bla</button> */}
         <Footer />
       </div>
     );
