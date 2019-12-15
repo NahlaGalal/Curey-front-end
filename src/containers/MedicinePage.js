3f73ee21ffcae0c23ed72ec9d81a573dcc2ed582
Mohamed Zayan <m7mdzayan@gmail.com>
2019-12-16 00:50:40 +0200

    medicine page finished but not responsive

---
+++ src/containers/MedicinePage.js
@@ -0,0 +1,71 @@
+import React from "react";
+import MedicineCard from "../components/Doctors and medications/MedicineCard";
+import Button from "../components/Button";
+import PharmacyItem from "../components/PharmacyItem";
+import LocationIcon from "../assets/svg/location.svg";
+
+const medicine = {
+  name: "Antinal",
+  price: 12,
+  isFavourite: false,
+  description:
+    "Broad-spectrum intestinal antiseptic for the treatment of diarrhea & gastroenteritis"
+};
+
+const MedicinePage = () => (
+  <React.Fragment>
+    <main className="medicinePage__contianer">
+      <div className="medicine__container">
+        <MedicineCard
+          name={medicine.name}
+          price={medicine.price}
+          description={medicine.description}
+          isFavourite
+        />
+      </div>
+
+      <div className="delivery__container">
+        <h3 className="heading-3">Delivery fees: 10 L.E</h3>
+        <p className="delivery__container__text">
+          Delivery fees are set based on the following address, and this is the
+          address that the medication will be delivered to
+        </p>
+        <span>
+          <img src={LocationIcon} alt="location-icon" /> Mansoura City, Gehan St
+        </span>
+        <Button className="btn btn-green btn-lg delivery__container__btn">
+          Change address
+        </Button>
+      </div>
+
+      <div className="pharmacies__container">
+        <PharmacyItem
+          name="Roshdy pharmacies"
+          rate={4}
+          reviews={12}
+          address="Roshdy pharmacies"
+        />
+        <PharmacyItem
+          name="Roshdy pharmacies"
+          rate={4}
+          reviews={12}
+          address="Roshdy pharmacies"
+        />
+        <PharmacyItem
+          name="Roshdy pharmacies"
+          rate={4}
+          reviews={12}
+          address="Roshdy pharmacies"
+        />
+        <PharmacyItem
+          name="Roshdy pharmacies"
+          rate={4}
+          reviews={12}
+          address="Roshdy pharmacies"
+        />
+      </div>
+    </main>
+  </React.Fragment>
+);
+
+export default MedicinePage;
--- src/containers/Router.js
+++ src/containers/Router.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-    BrowserRouter,
-    Route,
-    // Redirect,
-    Switch
+  BrowserRouter,
+  Route,
+  // Redirect,
+  Switch
 } from "react-router-dom";
 // Containers
 import Home from "./Home";
@@ -14,31 +14,29 @@ import Doctors from "./Doctors";
 import Medications from "./Medications";
 import Layout from "../components/Layout";
 import DoctorProfile from "./doctor-profile";
+import MedicinePage from "./MedicinePage";
 
 const Router = () => (
-    <BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <Route
+        exact
+        path={["/forgot-password", "/verification", "/reset-password"]}
+        component={Forgot}
+      />
+      <Route exact path="/signup" component={Signup} />
+      <Layout>
         <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route
-                exact
-                path={["/forgot-password", "/verification", "/reset-password"]}
-                component={Forgot}
-            />
-            <Route exact path="/signup" component={Signup} />
-            <Layout>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/doctors" component={Doctors} />
-                    <Route exact path="/medications" component={Medications} />
-                    <Route
-                        exact
-                        path="/doctor-profile/:id"
-                        component={DoctorProfile}
-                    />
-                </Switch>
-            </Layout>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/doctors" component={Doctors} />
+          <Route exact path="/medications" component={Medications} />
+          <Route exact path="/doctor-profile/:id" component={DoctorProfile} />
+          <Route exact path="/medicine" component={MedicinePage} />
         </Switch>
-    </BrowserRouter>
+      </Layout>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Router;
