dbf9ffbc1c31db2924a47d7703be0d1355652f4d
Mohamed Zayan <m7mdzayan@gmail.com>
2020-03-16 00:31:56 +0200

    add info to the medicine page

--- src/containers/MedicinePage.js
+++ src/containers/MedicinePage.js
@@ -6,65 +6,66 @@ import LocationIcon from "../assets/svg/location.svg";
 import ChangeAddress from "../components/Pop-ups/ChangeAddress";
 import { connect } from "react-redux";
 import * as actions from "../actions/types";
+import ReactLoading from "react-loading";
 
-const medicine = {
-  name: "Antinal",
-  price: 12,
-  isFavourite: false,
-  description:
-    "Broad-spectrum intestinal antiseptic for the treatment of diarrhea & gastroenteritis",
-  hovered: false,
-  pharmacies: [
-    {
-      name: "Roshdy pharmacies",
-      rate: 4,
-      reviews: 12,
-      address: "Mansoura City, Gehan St"
-    },
-    {
-      name: "Roshdy pharmacies",
-      rate: 4,
-      reviews: 12,
-      address: "Mansoura City, Gehan St"
-    },
-    {
-      name: "Roshdy pharmacies",
-      rate: 4,
-      reviews: 12,
-      address: "Mansoura City, Gehan St"
-    },
-    {
-      name: "Roshdy pharmacies",
-      rate: 4,
-      reviews: 12,
-      address: "Mansoura City, Gehan St"
-    },
-    {
-      name: "Roshdy pharmacies",
-      rate: 4,
-      reviews: 12,
-      address: "Mansoura City, Gehan St"
-    },
-    {
-      name: "Roshdy pharmacies",
-      rate: 4,
-      reviews: 12,
-      address: "Mansoura City, Gehan St"
-    },
-    {
-      name: "Roshdy pharmacies",
-      rate: 4,
-      reviews: 12,
-      address: "Mansoura City, Gehan St"
-    },
-    {
-      name: "Roshdy pharmacies",
-      rate: 4,
-      reviews: 12,
-      address: "Mansoura City, Gehan St"
-    }
-  ]
-};
+// const medicine = {
+//   name: "Antinal",
+//   price: 12,
+//   isFavourite: false,
+//   description:
+//     "Broad-spectrum intestinal antiseptic for the treatment of diarrhea & gastroenteritis",
+//   hovered: false,
+//   pharmacies: [
+//     {
+//       name: "Roshdy pharmacies",
+//       rate: 4,
+//       reviews: 12,
+//       address: "Mansoura City, Gehan St"
+//     },
+//     {
+//       name: "Roshdy pharmacies",
+//       rate: 4,
+//       reviews: 12,
+//       address: "Mansoura City, Gehan St"
+//     },
+//     {
+//       name: "Roshdy pharmacies",
+//       rate: 4,
+//       reviews: 12,
+//       address: "Mansoura City, Gehan St"
+//     },
+//     {
+//       name: "Roshdy pharmacies",
+//       rate: 4,
+//       reviews: 12,
+//       address: "Mansoura City, Gehan St"
+//     },
+//     {
+//       name: "Roshdy pharmacies",
+//       rate: 4,
+//       reviews: 12,
+//       address: "Mansoura City, Gehan St"
+//     },
+//     {
+//       name: "Roshdy pharmacies",
+//       rate: 4,
+//       reviews: 12,
+//       address: "Mansoura City, Gehan St"
+//     },
+//     {
+//       name: "Roshdy pharmacies",
+//       rate: 4,
+//       reviews: 12,
+//       address: "Mansoura City, Gehan St"
+//     },
+//     {
+//       name: "Roshdy pharmacies",
+//       rate: 4,
+//       reviews: 12,
+//       address: "Mansoura City, Gehan St"
+//     }
+//   ]
+// };
 
 class MedicinePage extends Component {
   state = { hovered: false, changeAddressBox: false, productID: 50 };
@@ -79,19 +80,29 @@ class MedicinePage extends Component {
         <main className="medicinePage__contianer">
           <div className="medicinePage__contianer__medicine">
             <div className="medicine__container">
-              <MedicineCard
-                name={medicine.name}
-                price={medicine.price}
-                description={medicine.description}
-                isFavourite={medicine.isFavourite}
-                onMouseMove={() => this.setState({ hovered: true })}
-                onMouseLeave={() => this.setState({ hovered: false })}
-                hovered={this.state.hovered}
-              />
+              {this.props.pharmacies.length ? (
+                <MedicineCard
+                  name={this.props.medicine.name}
+                  price={this.props.medicine.price}
+                  description={this.props.medicine.description}
+                  isFavourite={this.props.medicine.is_favourite}
+                  onMouseMove={() => this.setState({ hovered: true })}
+                  onMouseLeave={() => this.setState({ hovered: false })}
+                  hovered={this.state.hovered}
+                />
+              ) : (
+                <ReactLoading
+                  type="spokes"
+                  color="#0066ff"
+                  className="loading center mb-40"
+                />
+              )}
             </div>
 
             <div className="delivery__container">
-              <h3 className="heading-3">Delivery fees: 10 L.E</h3>
+              <h3 className="heading-3">
+                Delivery fees: {this.props.medicine.delivery_fees} L.E
+              </h3>
               <p className="delivery__container__text">
                 Delivery fees are set based on the following address, and this
                 is the address that the medication will be delivered to
@@ -111,19 +122,27 @@ class MedicinePage extends Component {
           <div className="medicinePage__contianer__pharmacy">
             <div className="pharmacies__container">
               <h2 className="heading-2">Pharmacies list</h2>
-              {medicine.pharmacies.map((pharmacy, i) => (
-                <PharmacyItem
-                  key={i}
-                  name={pharmacy.name}
-                  rate={pharmacy.rate}
-                  reviews={pharmacy.reviews}
-                  address={pharmacy.address}
-                  medication={{
-                    name: medicine.name,
-                    price: medicine.price
-                  }}
+              {this.props.pharmacies.length ? (
+                this.props.pharmacies.map((pharmacy, i) => (
+                  <PharmacyItem
+                    key={i}
+                    name={pharmacy.name}
+                    rate={pharmacy.overall_rating}
+                    reviews={10}
+                    address={pharmacy.address}
+                    medication={{
+                      name: this.props.medicine.name,
+                      price: this.props.medicine.price
+                    }}
+                  />
+                ))
+              ) : (
+                <ReactLoading
+                  type="spokes"
+                  color="#0066ff"
+                  className="loading center mb-40"
                 />
-              ))}
+              )}
             </div>
           </div>
           {this.state.changeAddressBox && (
@@ -138,7 +157,9 @@ class MedicinePage extends Component {
 }
 
 const mapStateToProps = state => ({
-  api_token: state.user.api_token
+  api_token: state.user.api_token,
+  medicine: state.medicationInfo.product,
+  pharmacies: state.medicationInfo.pharmacies
 });
 
 const mapDispatchToProps = dispatch => ({
