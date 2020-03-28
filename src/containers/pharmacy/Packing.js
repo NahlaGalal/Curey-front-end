d0b7ef9c5558acd221b34767a09d378206818df4
Nahla Galal <nahlaglal@gmail.com>
2020-03-29 01:11:50 +0200

    Add packing list page and modify navigation

---
+++ src/containers/pharmacy/Packing.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import OrderCard from "../../components/pharmacy/orderCard";
+import Button from "../../components/Button";
+
+class Packing extends Component {
+  state = {
+    medications: [
+      { name: "Flumox syrup", quantity: 3 },
+      { name: "Antinal pills", quantity: 2 },
+      { name: "Panadol extra pills", quantity: 1 }
+    ]
+  };
+  render() {
+    return (
+      <div className="pharamcyDashboardContainer">
+        <div className="dashboardGrid">
+          <OrderCard
+            name="MO Zayan"
+            address="Mansoura City, Gehan St"
+            day="JAN 23, 2020"
+            hour="4:30 PM"
+            medications={this.state.medications}
+            request={false}
+          />
+          <OrderCard
+            name="MO Zayan"
+            address="Mansoura City, Gehan St"
+            day="JAN 23, 2020"
+            hour="4:30 PM"
+            medications={this.state.medications}
+            request={false}
+          />
+          <OrderCard
+            name="MO Zayan"
+            address="Mansoura City, Gehan St"
+            day="JAN 23, 2020"
+            hour="4:30 PM"
+            medications={this.state.medications}
+            request={false}
+          />
+          <OrderCard
+            name="MO Zayan"
+            address="Mansoura City, Gehan St"
+            day="JAN 23, 2020"
+            hour="4:30 PM"
+            medications={this.state.medications}
+            request={false}
+          />
+          <OrderCard
+            name="MO Zayan"
+            address="Mansoura City, Gehan St"
+            day="JAN 23, 2020"
+            hour="4:30 PM"
+            medications={this.state.medications}
+            request={false}
+          />
+          <OrderCard
+            name="MO Zayan"
+            address="Mansoura City, Gehan St"
+            day="JAN 23, 2020"
+            hour="4:30 PM"
+            medications={this.state.medications}
+            request={false}
+          />
+        </div>
+        <Button className="btn btn-blue btn-lg">See more</Button>
+      </div>
+    );
+  }
+}
+
+export default Packing;
